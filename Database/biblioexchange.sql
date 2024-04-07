bee11fb4dd523048de054a995d2d6bd3d4ea11df
Paxterson32 <paxterson32@gmail.com>
2024-04-07 10:53:56 +0000

    modifications du fichier de BDD

--- Database/biblioexchange.sql
+++ Database/biblioexchange.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : dim. 31 mars 2024 à 21:55
+-- Généré le : dim. 07 avr. 2024 à 10:52
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
@@ -24,56 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `livres`
---
-
-DROP TABLE IF EXISTS `livres`;
-CREATE TABLE IF NOT EXISTS `livres` (
-  `id_livre` int NOT NULL AUTO_INCREMENT,
-  `titre_livre` varchar(100) NOT NULL,
-  `auteur` varchar(100) NOT NULL,
-  `année_de_publication` date NOT NULL,
-  PRIMARY KEY (`id_livre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `transactions`
---
-
-DROP TABLE IF EXISTS `transactions`;
-CREATE TABLE IF NOT EXISTS `transactions` (
-  `id_transaction` int NOT NULL AUTO_INCREMENT,
-  `nom_emetteur` varchar(100) NOT NULL,
-  `nom_recepteur` varchar(100) NOT NULL,
-  `id_livre` int NOT NULL,
-  `date-transaction` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `titre_livre` varchar(100) NOT NULL,
-  PRIMARY KEY (`id_transaction`),
-  KEY `id_livre` (`id_livre`),
-  KEY `id_livre_2` (`id_livre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user_sessions`
---
-
-DROP TABLE IF EXISTS `user_sessions`;
-CREATE TABLE IF NOT EXISTS `user_sessions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `session_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `user_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `session_id` (`session_id`,`user_id`),
-  UNIQUE KEY `session_id_2` (`session_id`,`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `utilisateurs`
 --
 
@@ -90,19 +40,20 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `niveau_etude` varchar(20) NOT NULL,
   `biographie` text NOT NULL,
   `genre_prefere` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `image_profil` varchar(100) NOT NULL,
+  `role_utilisateur` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   PRIMARY KEY (`id_utilisateur`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id_utilisateur`, `nom_utilisateur`, `prenom_utilisateur`, `email`, `mot_de_passe`, `date_naissance`, `telephone`, `sexe`, `niveau_etude`, `biographie`, `genre_prefere`) VALUES
-(24, 'Pierre', 'boss', 'example@gmail.com', '$2y$12$/Acx/fuMhHH0.Z5B1L7e7eQQZAwNpMh4a8ViNgjK8X2dOPA9//HHS', '2024-03-06', '11554352', 'homme', 'primaire', 'qdsdqsdsd', 'fiction'),
-(29, 'sdfdf', 'sdfsdfs', 'sdfsdfsdf@g.qsdqsd', '$2y$12$pExTd58JgW64XJrEEQKPeOHYbDGvABEE82h73W/HcV13vhnkW3aiO', '2024-03-08', 'qdsqdqsd', 'homme', 'primaire', 'ssssssssssssssssssssssssssssss', 'a:2:{i:0;s:14:\"scienceFiction\";i:1;s:11:\"fantastique\";}'),
-(32, 'sdfdf', 'sdfsdfs', 'a@a.a', '$2y$12$dYAdVXZTgBeQB6BLAGV7COOdrklow3dtHP3i4RTPnsnj/vxsU4J9e', '2024-03-08', 'qdsqdqsd', 'homme', 'primaire', 'ssssssssssssssssssssssssssssss', 'a:2:{i:0;s:14:\"scienceFiction\";i:1;s:11:\"fantastique\";}'),
-(33, 'tester', 'tester', 'tester@gmail.com', '$2y$12$OsIrm4gTy5mVb1WYgiPC9eIPz.HTuBpHV7IQd4Prk8KyXVxLUIbXq', '2024-03-08', '0645987853', 'homme', 'primaire', 'qlknqksnmqsjbsdojns ckl,qsd,qskmd,s qld,qmld,qsmld,qslùdsq', 'a:3:{i:0;s:7:\"fiction\";i:1;s:11:\"fantastique\";i:2;s:6:\"poesie\";}');
+INSERT INTO `utilisateurs` (`id_utilisateur`, `nom_utilisateur`, `prenom_utilisateur`, `email`, `mot_de_passe`, `date_naissance`, `telephone`, `sexe`, `niveau_etude`, `biographie`, `genre_prefere`, `image_profil`, `role_utilisateur`) VALUES
+(34, 'user', 'user', 'user@gmail.com', '$2y$12$yc6BBUz6c6/kdronEcfo5.wYuDTp2H66Mdfaylt9nrIJ2WN9Dr0fK', '2024-04-04', '0123456978', 'homme', 'primaire', ',qsdnsqjdsd dqdsodnqsdklsqd qdsnsdlqsdqskd,sqd dsqklkdsqldksqdk,sqldksnqdkqsdkl,sqdksq,dkqs,dklqsndjqsndkqsldqiuhdqmfdsfjpdf fsodfjsdkfezfi,sqpsf fsdofpkdsfodkfizjeofier zoirjzeoirjzeorizeorie eroizjrzeoirjeziorjze erzeoirjzeoirjezire ezirjriozjerioejzroi ezroiezjrzieorjezoijre', 'a:2:{i:0;s:7:\"fiction\";i:1;s:15:\"romanHistorique\";}', '', 'user'),
+(35, 'ESSOU', 'Pierre Canisius Pax', 'picanessou@gmail.com', '$2y$12$yOypcjtBuJAqkybzeaA9DunGe2NTdyKWLImuzYmf.6OOAM9.uq17y', '2001-12-21', '0645372871', 'homme', 'universitaire', 'Etudiant, Passionné de Basket et de belles créatures', 'a:4:{i:0;s:7:\"fiction\";i:1;s:14:\"scienceFiction\";i:2;s:11:\"fantastique\";i:3;s:6:\"poesie\";}', '', 'admin'),
+(37, 'toto', 'tata', 'toto@user.com', '$2y$12$dttzOlpuhv.luliaQxVi/uwDvAM9cI5VIwYHQUvW31hIj/ZuWKW5G', '2024-04-04', '0645372871', 'homme', 'primaire', 'qqlsdnlqslkdqs,dmqsdqsdqsdqsdqsdqsdqs qdqsdqsdqsdqsddqdsqd', 'a:2:{i:0;s:7:\"fiction\";i:1;s:14:\"scienceFiction\";}', '', 'user');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
